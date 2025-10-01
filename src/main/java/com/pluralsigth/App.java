f50863722a00e6c2ecd701c19d69ad75a32a4e5d
hckmsnbdy <hckmsnbdy@gmail.com>
2025-10-01 15:28:59 -0400

    -Methods added

---
+++ src/main/java/com/pluralsigth/App.java
@@ -0,0 +1,8 @@
+package com.pluralsigth;
+
+public class App {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/main/java/com/pluralsigth/TVRemote.java
+++ src/main/java/com/pluralsigth/TVRemote.java
@@ -1,5 +1,22 @@
 package com.pluralsigth;
 
 public class TVRemote {
+private String manufacturer;
+private int channelCurrent;
+private int CurrentVolume;
+private int powerState;
+
+    public void turnOn() {
+
+    }
+    public void turnOff() {}
+    public void changeChannel(int newCh) {}
+    public void adjustVolume(int newVolume) {}
+
+    public void getChannelName() {
+
+    }
+
+    {}
 
 }
